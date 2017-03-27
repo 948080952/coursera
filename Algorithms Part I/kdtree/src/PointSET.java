b084e09af8ec47e5868cee8eb0acb0533e00b931
DaiPei <daipei@hust.edu.cn>
2017-03-27 10:45:20 +0800

    add kdtree

---
+++ Algorithms Part I/kdtree/src/PointSET.java
@@ -0,0 +1,88 @@
+/**
+ * Created by daipei on 2017/3/26.
+ */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import java.lang.Math;
+
+public class PointSET {
+
+    private SET<Point2D> point2DS;
+    private int size;
+
+    public PointSET() {
+        point2DS = new SET<>();
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void insert(Point2D p) {
+        point2DS.add(p);
+    }
+
+    public boolean contains(Point2D p) {
+        for (Point2D point :
+                point2DS) {
+            if (point.equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void draw() {
+        for (Point2D point :
+                point2DS) {
+            point.draw();
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        SET<Point2D> rangeSet = new SET<>();
+
+        for (Point2D point :
+                point2DS) {
+            if (point.x() >= rect.xmin() &&
+                    point.x() <= rect.xmax() &&
+                    point.y() >= rect.ymin() &&
+                    point.y() <= rect.ymax()) {
+                rangeSet.add(point);
+            }
+        }
+        return rangeSet;
+    }
+
+    public Point2D nearest(Point2D p) {
+
+        Point2D nearestPoint = null;
+        double minDistance = 0;
+
+        for (Point2D point :
+                point2DS) {
+            double distance = p.distanceTo(point);
+            if (distance < minDistance) {
+                nearestPoint = point;
+            }
+        }
+        return nearestPoint;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
