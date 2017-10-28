59a271e9a8febb85274c36418369154f61d719e0
DaiPei <daipei@hust.edu.cn>
2017-10-28 20:39:20 +0800

    finish SAP.java

--- Algorithms/wordnet/src/SAP.java
+++ Algorithms/wordnet/src/SAP.java
@@ -4,6 +4,9 @@
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
     private Digraph digraph;
@@ -12,33 +15,50 @@ public class SAP {
         if (G == null) {
             throw new java.lang.IllegalArgumentException();
         }
-        digraph = new Digraph(G.V());
-        for (int i = 0; i < G.V(); i++) {
-            for (Integer j : G.adj(i)) {
-                digraph.addEdge(i, j);
-            }
-        }
+        digraph = new Digraph(G);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        if (v < 0 || w < 0 || v >= digraph.V() || w >= digraph.V()) {
+        if (!isValidId(v) || !isValidId(w)) {
             throw new java.lang.IllegalArgumentException();
         }
         BreadthFirstDirectedPaths p1 = new BreadthFirstDirectedPaths(digraph, v);
         BreadthFirstDirectedPaths p2 = new BreadthFirstDirectedPaths(digraph, w);
-        if (!p1.hasPathTo(w) && !p2.hasPathTo(v)) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < digraph.V(); i++) {
+            if (p1.hasPathTo(i) && p2.hasPathTo(i)) {
+                int l = p1.distTo(i) + p2.distTo(i);
+                if (l < min) {
+                    min = l;
+                }
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
             return -1;
         }
-        return Math.min(p1.distTo(w), p2.distTo(v));
+        return min;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        if (v < 0 || w < 0 || v >= digraph.V() || w >= digraph.V()) {
+        if (!isValidId(v) || !isValidId(w)) {
             throw new java.lang.IllegalArgumentException();
         }
-        return 0;
+        BreadthFirstDirectedPaths p1 = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths p2 = new BreadthFirstDirectedPaths(digraph, w);
+        int min = Integer.MAX_VALUE;
+        int anc = -1;
+        for (int i = 0; i < digraph.V(); i++) {
+            if (p1.hasPathTo(i) && p2.hasPathTo(i)) {
+                int l = p1.distTo(i) + p2.distTo(i);
+                if (l < min) {
+                    min = l;
+                    anc = i;
+                }
+            }
+        }
+        return anc;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -46,19 +66,25 @@ public class SAP {
         if (v == null || w == null) {
             throw new java.lang.IllegalArgumentException();
         }
+        for (Integer i : v) {
+            if (!isValidId(i)) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
+        for (Integer i : w) {
+            if (!isValidId(i)) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
         BreadthFirstDirectedPaths p1 = new BreadthFirstDirectedPaths(digraph, v);
         BreadthFirstDirectedPaths p2 = new BreadthFirstDirectedPaths(digraph, w);
         int min = Integer.MAX_VALUE;
-        for (Integer i : w) {
-            int l = p1.distTo(i);
-            if (l < min) {
-                min = l;
-            }
-        }
-        for (Integer i : v) {
-            int l = p2.distTo(i);
-            if (l < min) {
-                min = l;
+        for (int i = 0; i < digraph.V(); i++) {
+            if (p1.hasPathTo(i) && p2.hasPathTo(i)) {
+                int l = p1.distTo(i) + p2.distTo(i);
+                if (l < min) {
+                    min = l;
+                }
             }
         }
         if (min == Integer.MAX_VALUE) {
@@ -69,10 +95,53 @@ public class SAP {
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        if (v == null || w == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        for (Integer i : v) {
+            if (!isValidId(i)) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
+        for (Integer i : w) {
+            if (!isValidId(i)) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
+        BreadthFirstDirectedPaths p1 = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths p2 = new BreadthFirstDirectedPaths(digraph, w);
+        int min = Integer.MAX_VALUE;
+        int anc = -1;
+        for (int i = 0; i < digraph.V(); i++) {
+            if (p1.hasPathTo(i) && p2.hasPathTo(i)) {
+                int tmp = p1.distTo(i) + p2.distTo(i);
+                if (tmp < min) {
+                    min = tmp;
+                    anc = i;
+                }
+            }
+        }
+        return anc;
+    }
+
+    private boolean isValidId(int id) {
+        if (id >= 0 && id < digraph.V()) {
+            return true;
+        }
+        return false;
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 }
