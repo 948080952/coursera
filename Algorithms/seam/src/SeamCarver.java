2cc220813263ac5f3da6b1304042ad8e175c9e72
DaiPei <daipei@hust.edu.cn>
2017-11-10 09:54:09 +0800

    finish some simple function implementation

--- Algorithms/seam/src/SeamCarver.java
+++ Algorithms/seam/src/SeamCarver.java
@@ -6,23 +6,28 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 
-    public SeamCarver(Picture picture) {
+    private Picture picture;
 
+    public SeamCarver(Picture picture) {
+        this.picture = picture;
     }
 
     public Picture picture() {
-
+        return picture;
     }
 
     public int width() {
-        return 0;
+        return picture.width();
     }
 
     public int height() {
-        return 0;
+        return picture.height();
     }
 
     public double energy(int x, int y) {
+        validateCol(x);
+        validateRow(y);
+
         return 0;
     }
 
@@ -45,4 +50,24 @@ public class SeamCarver {
     public void removeVerticalSeam(int[] seam) {
 
     }
+
+    private void validateRow(int row) {
+        if (row < 0 || row >= picture.height()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateCol(int col) {
+        if (col < 0 || col >= picture.width()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isBorder(int x, int y) {
+        if (x == 0 || y == 0 || x == picture.width() - 1 || y == picture.height() - 1) {
+            return true;
+        }
+        return false;
+    }
+
 }
