899cbe9bae10af10944ac0370c47e5613c3c654a
DaiPei <daipei@hust.edu.cn>
2017-11-10 09:29:54 +0800

    create SeamCarver.java

---
+++ Algorithms/seam/src/SeamCarver.java
@@ -0,0 +1,48 @@
+/**
+ * Created by daipei on 2017/11/10.
+ */
+
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+
+    public SeamCarver(Picture picture) {
+
+    }
+
+    public Picture picture() {
+
+    }
+
+    public int width() {
+        return 0;
+    }
+
+    public int height() {
+        return 0;
+    }
+
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    public int[] findHorizontalSeam() {
+        int[] seams;
+
+        return seams;
+    }
+
+    public int[] findVerticalSeam() {
+        int[] seams;
+
+        return seams;
+    }
+
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+}
