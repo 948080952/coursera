da56fc66e9b39a0a8f407a45c7fa357f4d351528
DaiPei <daipei@hust.edu.cn>
2017-11-21 23:32:36 +0800

    add three file and make it pass the test

---
+++ Algorithms/boggle/src/BoggleSolver.java
@@ -0,0 +1,118 @@
+/**
+ * Created by daipei on 2017/11/21.
+ */
+
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BoggleSolver {
+
+    private TSET allWords;
+
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+        allWords = new TSET(dictionary);
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        SET<String> validWords = new SET<>();
+        boolean[][] mark;
+        for (int i = 0; i < board.cols(); i++) {
+            for (int j = 0; j < board.rows(); j++) {
+                mark = new boolean[board.cols()][board.rows()];
+                dfs(board, new StringBuilder(), validWords, i, j, mark);
+            }
+        }
+        return validWords;
+    }
+
+    private void dfs(BoggleBoard board, StringBuilder string, SET<String> collecion, int x, int y, boolean[][] mark) {
+        mark[x][y] = true;
+        string.append(board.getLetter(x, y));
+        if (!allWords.containPrefix(string.toString())) {
+            string.deleteCharAt(string.length() - 1);
+            mark[x][y] = false;
+            return;
+        }
+        if (allWords.contain(string.toString())) {
+            collecion.add(string.toString());
+        }
+        int w = board.cols(), h = board.rows();
+        int x1, y1 = y;
+        x1 = x - 1;
+        if (x1 >= 0 && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        y1 = y - 1;
+        if (y1 >= 0 && x1 >= 0 && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        x1 = x;
+        if (y1 >= 0 && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        x1 = x + 1;
+        if (y1 >= 0 && x1 < w && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        y1 = y;
+        if (x1 < w && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        y1 = y + 1;
+        if (x1 < w && y1 < h && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        x1 = x;
+        if (y1 < h && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        x1 = x - 1;
+        if (x1 >= 0 && y1 < h && !mark[x1][y1]) {
+            dfs(board, string, collecion, x1, y1, mark);
+        }
+        string.deleteCharAt(string.length() - 1);
+        mark[x][y] = false;
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        if (allWords.contain(word)) {
+            return score(word);
+        }
+        return 0;
+    }
+
+    private int score(String word) {
+        if (word.length() <= 2) {
+            return 0;
+        } else if (word.length() <= 4) {
+            return 1;
+        } else if (word.length() <= 5) {
+            return 2;
+        } else if (word.length() <= 6) {
+            return 3;
+        } else if (word.length() <= 7) {
+            return 5;
+        } else {
+            return 11;
+        }
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
+}
---
+++ Algorithms/boggle/src/TSET.java
@@ -0,0 +1,101 @@
+/**
+ * Created by daipei on 2017/11/21.
+ */
+
+import edu.princeton.cs.algs4.In;
+
+public class TSET {
+
+    private Node[] roots; // root of TSET
+
+    private static class Node {
+        private char c;
+        private boolean isString = false;
+        private Node left, mid, right;
+    }
+
+    public TSET() {
+        roots = new Node[26];
+    }
+
+    public TSET(String[] strings) {
+        roots = new Node[26];
+        for (String string : strings) {
+            insert(string);
+        }
+    }
+
+    public boolean contain(String string) {
+        if (string == null) {
+            throw new IllegalArgumentException();
+        }
+        Node node = get(string);
+        return node != null && node.isString;
+    }
+
+    public boolean containPrefix(String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException();
+        }
+        Node node = get(prefix);
+        return node != null;
+    }
+
+    private Node get(String key) {
+        if (key == null) throw new IllegalArgumentException();
+        if (key.length() == 0) throw new IllegalArgumentException();
+        char c = key.charAt(0);
+        return get(roots[c - 'A'], key, 0);
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        char c = key.charAt(d);
+        if (c < x.c)                    return get(x.left, key, d);
+        else if (c > x.c)               return get(x.right, key, d);
+        else if (d < key.length() - 1)  return get(x.mid, key, d + 1);
+        else                            return x;
+    }
+
+    public void insert(String string) {
+        if (string == null) {
+            throw new IllegalArgumentException();
+        }
+        char c = string.charAt(0);
+        roots[c - 'A'] = insert(roots[c - 'A'], string, 0);
+    }
+
+    private Node insert(Node x, String string, int d) {
+        char c = string.charAt(d);
+        if (x == null) {
+            x = new Node();
+            x.c = c;
+        }
+        if (c < x.c)                        x.left = insert(x.left, string, d);
+        else if (c > x.c)                   x.right = insert(x.right, string, d);
+        else if (d < string.length() - 1)   x.mid = insert(x.mid,string, d + 1);
+        else                                x.isString = true;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        In in2 = new In(args[1]);
+        String[] dic2 = in2.readAllStrings();
+        TSET tset = new TSET(dictionary);
+        for (String string : dictionary) {
+            assert tset.contain(string);
+        }
+        for (String string : dic2) {
+            if (tset.contain(string)) {
+                System.out.println("contain: " + string);
+            }
+            if (tset.containPrefix(string)) {
+                System.out.println("contain prefix: " + string);
+            }
+        }
+        System.out.println("finish!");
+    }
+
+}
